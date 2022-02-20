eedc6683f72bd1155193d7327186c4f5984c4d8d
lesrz <suarezarnaldi.leo@gmail.com>
2022-02-20 01:19:48 -0300

    Add material button toggle module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { FormsModule } from '@angular/forms';
 
-const MaterialComponents = [MatButtonModule, MatSidenavModule, FormsModule];
+const MaterialComponents = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatSidenavModule,
+  FormsModule,
+];
 
 @NgModule({
   imports: [MaterialComponents],
