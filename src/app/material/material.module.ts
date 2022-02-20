883b505322ac004e7432f5c3d3765acc16ff2c0c
lesrz <suarezarnaldi.leo@gmail.com>
2022-02-20 16:13:11 -0300

    Add grid and expansion panel imports

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { FormsModule } from '@angular/forms';
 
 const MaterialComponents = [
@@ -9,6 +11,8 @@ const MaterialComponents = [
   MatButtonToggleModule,
   MatSidenavModule,
   FormsModule,
+  MatExpansionModule,
+  MatGridListModule,
 ];
 
 @NgModule({
