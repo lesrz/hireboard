dcc625a16bd642087643cfb8bf62dca424391b45
lesrz <suarezarnaldi.leo@gmail.com>
2022-02-22 20:40:19 -0300

    Create add dialog component

--- src/app/components/shared/add-dialog/add-dialog.component.ts
+++ src/app/components/shared/add-dialog/add-dialog.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Employee } from 'src/app/employee';
+import { EmployeeService } from 'src/app/employee.service';
+import { DialogData } from '../dialog-data';
 
 @Component({
   selector: 'app-add-dialog',
   templateUrl: './add-dialog.component.html',
-  styleUrls: ['./add-dialog.component.scss']
+  styleUrls: ['./add-dialog.component.scss'],
 })
 export class AddDialogComponent implements OnInit {
+  constructor(
+    public employeeService: EmployeeService,
+    public dialogRef: MatDialogRef<AddDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
+  onAddEmployee(addForm: NgForm): void {
+    this.employeeService.addEmployee(addForm.value).subscribe({
+      next: (response: Employee) => {
+        console.log(response);
+        addForm.reset();
+        this.dialogRef.close();
+      },
+      error: (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      },
+      complete: () => {
+        this.dialogRef.close();
+      },
+    });
+  }
 }
