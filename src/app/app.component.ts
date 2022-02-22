281d26a6b80939290dae92ad4d87b4c40aa5a663
lesrz <suarezarnaldi.leo@gmail.com>
2022-02-22 01:09:19 -0300

    Set up dialog functions on app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { AddDialogComponent } from './components/shared/add-dialog/add-dialog.component';
+import { DeleteDialogComponent } from './components/shared/delete-dialog/delete-dialog.component';
+import { EditDialogComponent } from './components/shared/edit-dialog/edit-dialog.component';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -12,7 +16,10 @@ export class AppComponent implements OnInit {
   opened = false;
   public employees: Employee[] = [];
 
-  constructor(private employeeService: EmployeeService) {}
+  constructor(
+    private employeeService: EmployeeService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.getEmployees();
@@ -27,5 +34,25 @@ export class AppComponent implements OnInit {
         alert(error.message);
       }
     );
+  }
+
+  public openDialog(dialogType: string): void {
+    const dialogConfig: MatDialogConfig = new MatDialogConfig();
+
+    dialogConfig.autoFocus = false;
+
+    switch (dialogType) {
+      case 'add':
+        this.dialog.open(AddDialogComponent, dialogConfig);
+        break;
+      case 'edit':
+        this.dialog.open(EditDialogComponent, dialogConfig);
+        break;
+      case 'delete':
+        this.dialog.open(DeleteDialogComponent, dialogConfig);
+        break;
+      default:
+        console.log('No dialog pop-up for this button');
+    }
   }
 }
